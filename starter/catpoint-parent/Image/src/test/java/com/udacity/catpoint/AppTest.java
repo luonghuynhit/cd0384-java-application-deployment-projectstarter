cc5df8d5a7ee59ba13f5652760a296aa1e47f873
luonght <luonght@LPP00215048F.fsoft.fpt.vn>
2024-09-11 23:11:01 +0700

    split 2 modules OK
    split code into 2 modules: Image & Security OK ==> build OK, run module Security OK

---
+++ starter/catpoint-parent/Image/src/test/java/com/udacity/catpoint/AppTest.java
@@ -0,0 +1,10 @@
+package com.udacity.catpoint;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+}
---
+++ starter/catpoint-parent/Security/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.catpoint.security {
+    requires java.desktop;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+    requires com.udacity.catpoint.image;
+    requires miglayout.swing;
+    exports com.udacity.catpoint.security;
+}
